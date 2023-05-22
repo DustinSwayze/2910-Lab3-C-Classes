0d21c6986cd476c94ce99c0e4be02d9078c67eed
Dustin Swayze <ZDCS16@ETSU.EDU>
2023-05-22 12:44:06 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2910_Lab3_C__Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
